e5506ea6a932e4d74bb17812d6706a796c8d12ac
taticode <tatiana.gl.3011@gmail.com>
2026-05-02 20:37:11 +0200

    fix routes next footer

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { ExperienceComponent } from '../experience/experience.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { ContactComponent } from '../contact/contact.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class Home {
+export class HomeComponent {
 
 }
